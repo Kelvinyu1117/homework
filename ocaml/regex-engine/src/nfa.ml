788fc711f79848b7c9c9dc563123c956711dc47f
Kelvin Wu <kelvinyu1117@gmail.com>
2024-03-12 20:55:58 +0800

    hw: ocaml project - regex engine: complete new_states(), new_trans(), new_finals(), nfa_to_dfa_step(), nfa_to_dfa(), regexp_to_nfa()

--- ocaml/regex-engine/src/nfa.ml
+++ ocaml/regex-engine/src/nfa.ml
@@ -29,16 +29,6 @@ let explode (s: string) : char list =
 (****************)
 (* Part 1: NFAs *)
 (****************)
-
-let rec find_transition state symbol delta =
-  match delta with
-  | [] -> None
-  | (from, t, dest) :: tail -> 
-      match (t, symbol) with
-      | (Some v, Some symbol) when from = state && v = symbol -> Some dest
-      | (None, None) when from = state -> Some dest
-      | _ -> find_transition state symbol tail
-
 let rec find_all_transition state symbol delta =
   match delta with
   | [] -> []
@@ -51,12 +41,10 @@ let rec find_all_transition state symbol delta =
 let move (nfa: ('q,'s) nfa_t) (qs: 'q list) (s: 's option) : 'q list =
   let rec move_imp nfa qs s =
     match qs with
-      | [] -> []
-      | head :: tail ->
-          let result = find_transition head s nfa.delta in
-            match result with
-            | Some v -> v :: (move_imp nfa tail s)
-            | _ -> move_imp nfa tail s
+    | [] -> []
+    | head :: tail ->
+      let result = find_all_transition head s nfa.delta in
+      union result (move_imp nfa tail s)
   in
   move_imp nfa qs s
 
@@ -76,33 +64,61 @@ let e_closure (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list =
                 union result (e_closure_imp nfa tail new_visited)
   in
     e_closure_imp nfa qs []
+
 let accept (nfa: ('q,char) nfa_t) (s: string) : bool =
-  let rec accept_imp (nfa: ('q, char) nfa_t) (qs: 'q list) (s_list: char list) (visited: 'q list) =
-    match (qs, s_list) with
-    | [], _ -> false
-    | _, [] -> intersection visited nfa.fs != []
-    | (q :: qs', head :: tail) ->
-      let next_states = e_closure nfa (move nfa [q] (Some head)) in
-      accept_imp nfa next_states tail (union visited next_states)
+  let rec accept_imp current_states s_list =
+    match s_list with
+    | [] -> [] != intersection current_states nfa.fs
+    | head :: tail ->
+      let next_states = List.fold_left (fun acc state ->
+          union acc (find_all_transition state (Some head) nfa.delta)
+        ) [] current_states
+      in
+      let epsilon_closure = e_closure nfa next_states in
+      accept_imp epsilon_closure tail
   in
-  accept_imp nfa [nfa.q0] (explode s) [nfa.q0]
+  accept_imp (e_closure nfa [nfa.q0]) (explode s)
 
 (*******************************)
 (* Part 2: Subset Construction *)
 (*******************************)
 
+let rec follow_epsilon nfa lst c = 
+  let next_states = List.fold_left (fun acc q -> union acc (find_all_transition q (Some c) nfa.delta)) [] lst in
+    e_closure nfa next_states
+
 let new_states (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
-  failwith "unimplemented"
+  List.map (fun c -> follow_epsilon nfa qs c) nfa.sigma
+
 
 let new_trans (nfa: ('q,'s) nfa_t) (qs: 'q list) : ('q list, 's) transition list =
-  failwith "unimplemented"
+  List.map (fun c -> (qs, Some c, follow_epsilon nfa qs c)) nfa.sigma
 
 let new_finals (nfa: ('q,'s) nfa_t) (qs: 'q list) : 'q list list =
-  failwith "unimplemented"
+  if List.exists (fun q -> elem q nfa.fs) qs then [qs]
+  else [] 
 
 let rec nfa_to_dfa_step (nfa: ('q,'s) nfa_t) (dfa: ('q list, 's) nfa_t)
     (work: 'q list list) : ('q list, 's) nfa_t =
-  failwith "unimplemented"
+    match work with
+    | [] -> dfa
+    | head :: tail ->
+      let possible_states = List.filter (fun state -> state != [] && not (elem state dfa.qs)) (new_states nfa head) in
+      let new_transitions = List.filter (fun (from, t, dest) -> dest != []) (new_trans nfa head) in
+      let new_finals = new_finals nfa head in
+      let new_dfa = {
+        qs = union possible_states dfa.qs;
+        sigma = dfa.sigma;
+        delta = union new_transitions dfa.delta;
+        q0 = dfa.q0;
+        fs = union dfa.fs new_finals
+      } in
+        nfa_to_dfa_step nfa new_dfa (union possible_states tail)
 
 let nfa_to_dfa (nfa: ('q,'s) nfa_t) : ('q list, 's) nfa_t =
-  failwith "unimplemented"
+  let q0 = e_closure nfa [nfa.q0] in
+  let dfa = { qs = [q0]; sigma = nfa.sigma; delta = []; q0 = q0; fs = []} in
+  let work = [q0] in
+    nfa_to_dfa_step nfa dfa work
+  
+
--- ocaml/regex-engine/test/student/student.ml
+++ ocaml/regex-engine/test/student/student.ml
@@ -3,6 +3,20 @@ open OUnit2
 open P3.Nfa
 open P3.Regexp
 
+let assert_set_set_eq lst1 lst2 =
+  let es l = List.sort_uniq compare (List.map (List.sort compare) l) in
+  let sorted_lst1 = es lst1 in
+  let sorted_lst2 = es lst2 in
+
+  (* Debug printing *)
+  print_endline "---- Debug Printing ----";
+  print_endline "lst1:";
+  List.iter (fun l -> List.iter (fun x -> print_int x; print_string " ") l; print_newline ()) sorted_lst1;
+  print_endline "lst2:";
+  List.iter (fun l -> List.iter (fun x -> print_int x; print_string " ") l; print_newline ()) sorted_lst2;
+  print_endline "------------------------";
+
+  assert_equal sorted_lst1 sorted_lst2
 let student_test1 _ = 
   let m3 =
     { qs= [0; 1; 2; 3]
@@ -21,9 +35,46 @@ let student_test1 _ =
   assert_nfa_closure m3 [2] [2] ;
   assert_nfa_closure m3 [3] [3] ;
 ;;
+let student_test2 _ = 
+let m1 =
+  { qs= [0; 1; 2; 3; 4]
+  ; sigma= ['a'; 'b']
+  ; delta= [(0, Some 'a', 1); (0, Some 'a', 2); (2, Some 'b', 3); (2, None, 4); (4, Some 'a', 4)]
+  ; q0= 0
+  ; fs= [1; 3] } in
+  assert_set_set_eq [[]; []] (new_states m1 []) ;
+  assert_set_set_eq [[1; 2; 4]; []] (new_states m1 [0]) ;
+  assert_set_set_eq [[4]; []] (new_states m1 [3; 4]) ;
+  assert_set_set_eq [[1; 2; 4]; [3]] (new_states m1 [0; 2]) ;
+  assert_set_set_eq [[1; 2; 4]; [3]] (new_states m1 [0; 1; 2; 3])
 
+let student_test3 _ = 
+  let m1 =
+    { qs= [0; 1; 2; 3; 4]
+    ; sigma= ['a'; 'b']
+    ; delta= [(0, Some 'a', 1); (0, Some 'a', 2); (2, Some 'b', 3); (2, None, 4); (4, Some 'a', 4)]
+    ; q0= 0
+    ; fs= [1; 3] } in
+    assert_trans_eq
+      [([0], Some 'a', [1; 2; 4]); ([0], Some 'b', [])]
+      (new_trans m1 [0]) ;
+    assert_trans_eq
+      [([0; 2], Some 'a', [1; 2; 4]); ([0; 2], Some 'b', [3])]
+      (new_trans m1 [0; 2])
+
+let student_test4 _ = 
+  let m1 = regexp_to_nfa @@ string_to_regexp "a*b" in
+  print_endline (str_of_int_fsm m1);
+  assert_nfa_deny m1 "a";
+  assert_nfa_accept m1 "b";
+  assert_nfa_accept m1 "ab";
+  assert_nfa_accept m1 "aab";
+;;
 let suite = "student" >::: [ 
-  "student_test1" >:: student_test1 
+  "student_test1" >:: student_test1;
+  "student_test2" >:: student_test2;
+  "student_test3" >:: student_test3;
+  "student_test4" >:: student_test4;
 ]
 
 let _ = run_test_tt_main suite
